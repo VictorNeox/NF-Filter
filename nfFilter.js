46f97ccafe40a0a61428e8e2670cc3e5356e5a04
Victor Neox <victorrodrigues753@gmail.com>
2020-11-13 08:26:45 -0300

    Improvements

--- nfFilter.js
+++ nfFilter.js
@@ -1,18 +1,23 @@
-let notas = document.querySelectorAll('[data-toggle="modal"]');
-let count = 0;
-notas = Array.from(notas);
 
-notas.forEach(function(button) {
-    if(count % 2 == 0) {
-        let chaveTransf = $(button).parent().next('td').text();
-        let chaveVenda = $(button).parent()
-            .next('td')
-            .next('td')
-            .next('td')
-            .text();
-        if(chaveTransf && chaveVenda) {
-            $(button).parent().parent().remove();
+$( "#todos_pedidos3" )
+    .after('<button style="margin-left: 5px" type="button" class="btn btn-primary" id="todos_pedidos4">Notas sem chave</button>');
+
+// Retira todas as notas que possuem uma chave
+$("#todos_pedidos4").on("click", function(e) {
+    let notas = document.querySelectorAll('[data-toggle="modal"]');
+    let count = 0;
+    notas.forEach((button) => {
+        if(count % 2 == 0) {
+            let chaveTransf = $(button).parent().next('td').text();
+            let chaveVenda = $(button).parent()
+                .next('td')
+                .next('td')
+                .next('td')
+                .text();
+            if(chaveTransf && chaveVenda) {
+                $(button).parent().parent().remove();
+            }
         }
-    }
-    count++;
+        count++;
+    });
 });
